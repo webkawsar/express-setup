f2aa4a37001cccfba0bdf28f3263074ba93a1ffd
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-05-22 11:04:06 +0600

    added database connection by mongoose

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+require('dotenv').config();
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+const connectDB = require('./config/db');
+
+// Database Connection
+connectDB();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,6 +32,7 @@ app.use((req, res, next) => {
 });
 
 // error handler
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
@@ -38,4 +43,8 @@ app.use((err, req, res, next) => {
     res.render('error');
 });
 
-module.exports = app;
+// app port listening
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+});
